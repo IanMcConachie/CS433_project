6b6e6c774e934cdadd8007c0982f931d3c1969d2
IanMcConachie <email.mcconachie@gmail.com>
2023-05-28 13:33:58 -0700

    Added Padding
    Added padding to make msg length the same for David's steganography protocol.

--- crypto.py
+++ crypto.py
@@ -1,7 +1,7 @@
 """
 Author:             Ian McConachie
 Date Created:       05.04.2023
-Last Date Modified:	05.06.2023
+Last Date Modified:	05.28.2023
 
 This file defines the functions necessary for the cryptographic component of
 Stegosaurus. See cryptography.md in the docs folder for more detail.
@@ -11,6 +11,7 @@ Stegosaurus. See cryptography.md in the docs folder for more detail.
 
 # 16-byte key (In future, we will get from DB)
 key = b'0123456701234567'
+cons_iv = b'0123456701234567'	# This is like a random seed for CBC mode
 # The constant watermark that will be embedded in plaintext onto all imgs
 cons_wm = '304b475d1a0d5eba9fc7e3d821076c8bc0f33b813d42977a3dc1902b64924cc8'
 
@@ -20,20 +21,20 @@ cons_wm = '304b475d1a0d5eba9fc7e3d821076c8bc0f33b813d42977a3dc1902b64924cc8'
 from Crypto.Cipher import AES
 
 
-## Modular Functions
+## Encryption Functions
 
 def AES_setup(key):
 	"""
 	:inputs:    key     [bytes]
-	:returns:   AES     [AES obj]
-	            nonce   [bytes]
+	:returns:   cipher  [AES obj]
 
 	This function sets up an AES cryptosystem after being given the key with
 	which to base the symmetric encryption on.
 	"""
-	cipher = AES.new(key, AES.MODE_EAX) 	# Init AES system
-	num_once = cipher.nonce		            # nonce = number used once
-	return cipher, num_once
+	cipher = AES.new(key, AES.MODE_CBC, iv=cons_iv) 	# Init AES system (using CBC mode for now)
+	return cipher
+
+## Encryption Functions
 
 def AES_encrypt(cipher, data):
 	"""
@@ -46,10 +47,9 @@ def AES_encrypt(cipher, data):
 	encrypted form of the data by performing encryption with the inputted 
 	cipher object.
 	"""
-	c_text, tag = cipher.encrypt_and_digest(data)
+	c_text = cipher.encrypt(data)
 	c_text = c_text.hex()
-	tag = tag.hex()
-	return c_text, tag	
+	return c_text
 
 def Assemble_msg(cipher, c_data, p_data):
 	"""
@@ -62,9 +62,9 @@ def Assemble_msg(cipher, c_data, p_data):
 	want to embed as plaintext (p_data) and a cryptosystem (cipher). With these
 	it generates a message to be embedded into an image using steganography.
 	"""
-	c_text, tag = AES_encrypt(cipher, c_data)
+	c_text = AES_encrypt(cipher, c_data)
 	p_data = p_data.hex()
-	msg = c_text + tag + p_data + cons_wm		# do we need to include the tag here?
+	msg = c_text + p_data + cons_wm
 	return msg
 
 def generate_msg(key, c_data, p_data):
@@ -78,9 +78,74 @@ def generate_msg(key, c_data, p_data):
 	Assemble_msg functions from above into one function for ease of use with
 	Flask framework.
 	"""
-	cipher, num_once = AES_setup(key)
+	padding = b'0000000000000000'
+	cipher = AES_setup(key)
 	msg = Assemble_msg(cipher, c_data, p_data)
+	msg = msg + padding.hex()
 	return msg
+
+
+## Decryption Functions
+
+def split_msg(msg):
+	"""
+	:inputs:    msg        [string]
+	:returns:   c_text     [string]
+	            p_data     [string]
+	            watermark  [string]
+
+	This is a relatively simple function that splits 
+	"""
+	c_text = msg[0:256]      # The first 256 chars are ciphertext
+	p_data = msg[256:384]    # The next 128 chars are plaintext 
+	watermark = msg[384:512] # The last 128 chars are the constant
+	return c_text, p_data, watermark
+
+
+
+def AES_decrypt(msg, cipher):
+	"""
+	:inputs:    cipher     [AES obj]
+	:returns:	plaintext  [bytes]
+
+	This function decrypts a cipher text message and returns the plaintext
+	in the form of bytes.
+	"""
+	msg = bytes.fromhex(msg)
+	plaintext = cipher.decrypt(msg)
+	plaintext = plaintext.hex()
+	return plaintext
+
+
+def interpret_msg(msg):
+	"""
+	:inputs:    msg        [string]
+	:returns:	is_steg    [bool]
+	            user_id    [bytes]
+	            pt_match   [bool]
+
+	This function takes in a full msg from an image and interprets it in all
+	the relevant ways: it sees if the constant watermark is there, it extracts
+	the user_id, and it sees if the decrypted section matches the plaintext.
+	"""
+	is_steg = False
+	pt_match = False
+
+	cipher = AES_setup(key)
+	c_text, p_text, watermark = split_msg(msg)
+	ct_to_pt = AES_decrypt(c_text, cipher)
+	hash_val = bytes.fromhex(ct_to_pt[0:128])
+	pt_dcryp = ct_to_pt[128:]
+
+
+	# Check to see if the constant watermark is there
+	if (watermark == cons_wm):
+		is_steg = True
+	# Check to see if plaintext matches decrypted section
+	if (pt_dcryp == p_text):
+		pt_match = True
+
+	return is_steg, hash_val, pt_match
 
 
 ## Main Function
@@ -90,6 +155,12 @@ def main():
 	p_data = b'304b475d1a0d5eba9fc7e3d821076c8bc0f33b813d42977a3dc1902b64924cc8'
 	msg = generate_msg(key, c_data, p_data)
 	print(msg)
+	is_steg, hash_val, pt_match = interpret_msg(msg)
+	
+	print("\nConstant watermark recovered successfully? \t", is_steg)
+	print("Decrypted data match original text? \t\t", pt_match)
+	print("\nClaimed user id:\n\t", hash_val, "\n")
+	
 	return msg
 
 
